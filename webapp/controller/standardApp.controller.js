cf74887212a4349f16d425dc28126a5ca06c347a
Dhaliwal <tajinder.dhaliwal@accenture.com>
2024-08-20 10:43:41 +0200

    Added filters

--- webapp/controller/standardApp.controller.js
+++ webapp/controller/standardApp.controller.js
@@ -1,12 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
 ],
-function (Controller) {
+function (Controller,JSONModel,MessageToast,Filter,FilterOperator) {
     "use strict";
 
     return Controller.extend("standardapp.controller.standardApp", {
         onInit: function () {
 
+  
+
+        },
+        onSearch: function(){
+            
+            var customerKeys = this.byId('filterCustomer').getControl().getSelectedKeys();
+            var customerFilters = customerKeys.map(function(selectedKey){
+                    return new Filter({
+                        path : 'CustomerID',
+                        operator: FilterOperator.Contains,
+                        value1 : selectedKey
+                    });                
+                ;
+            });
+            this.getView().byId('ordersTable').getBinding("items").filter(customerFilters);
+
+
         }
+
+
+
     });
 });
